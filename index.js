cff785a39b9b04f8c4e6b2f9a75e97a276dff3e0
tim-kos <tim@debuggable.com>
2011-11-29 11:12:24 +0100

    adding support for weeks for nodejs

--- index.js
+++ index.js
@@ -1,67 +1,91 @@
 module.exports = {
+  hasOne   : false,
+  hasYear  : false,
+  hasMonth : false,
+  hasWeek  : false,
+  hasDay   : false,
+  YEAR     : 31536000,
+  MONTH    : 2592000,
+  WEEK     : 604800,
+  DAY      : 86400,
+  HOUR     : 3600,
+  MINUTE   : 60,
+
   agoInWords: function(date, now) {
+    this.rewind();
+
     if (!now) now = +new Date() / 1000;
     var then = +new Date(date) / 1000;
 
     var diff = now - then;
 
-    var years = Math.floor(diff / 31536000);
-    diff -= years * 31536000;
+    var years = Math.floor(diff / this.YEAR);
+    diff -= years * this.YEAR;
 
-    var months = Math.floor(diff / 2592000);
-    diff -= months * 2592000;
+    var months = Math.floor(diff / this.MONTH);
+    diff -= months * this.MONTH;
 
-    var days = Math.floor(diff / 86400);
-    diff -= days * 86400;
+    var weeks = Math.floor(diff / this.WEEK);
+    diff -= weeks * this.WEEK;
 
-    var hours = Math.floor(diff / 3600);
-    diff -= hours * 3600;
+    var days = Math.floor(diff / this.DAY);
+    diff -= days * this.DAY;
 
-    var minutes = Math.floor(diff / 60);
-    diff -= minutes * 60;
+    var hours = Math.floor(diff / this.HOUR);
+    diff -= hours * this.HOUR;
+
+    var minutes = Math.floor(diff / this.MINUTE);
+    diff -= minutes * this.MINUTE;
 
     var r = '';
-    var hasOne = hasYear = hasMonth = hasDay = false;
-
     if (years > 0) {
-      if (hasOne) r += ', ';
-      r += this.pluralize(years, 'year');
-      hasOne = true;
-      hasYear = true;
+      r += this.sep() + this.pluralize(years, 'year');
+      this.hasYear = true;
     }
 
     if (months > 0) {
-      if (hasOne) r += ', ';
-      r += this.pluralize(months, 'month');
-      hasOne = true;
-      hasMonth = true;
+      r += this.sep() + this.pluralize(months, 'month');
+      this.hasMonth = true;
     }
 
-    if (!hasYear && days > 0) {
-      if (hasOne) r += ', ';
-      r += this.pluralize(days, 'day');
-      hasOne = true;
-      hasDay = true;
+    if (weeks > 0 && !this.hasYear) {
+      r += this.sep() + this.pluralize(weeks, 'week');
+      this.hasWeek = true;
     }
 
-    if (!hasYear && !hasMonth && hours > 0) {
-      if (hasOne) r += ', ';
-      r += this.pluralize(hours, 'hour');
-      hasOne = true;
+    if (days > 0 && !this.hasYear && !this.hasMonth) {
+      r += this.sep() + this.pluralize(days, 'day');
+      this.hasDay = true;
     }
 
-    if (!hasYear && !hasMonth && !hasDay && minutes > 0) {
-      if (hasOne) r += ', ';
-      r += this.pluralize(minutes, 'minute');
-      hasOne = true;
+    if (hours > 0 && !this.hasYear && !this.hasMonth && !this.hasWeek) {
+      r += this.sep() + this.pluralize(hours, 'hour');
     }
 
-    if (hasOne) return r + ' ago';
+    if (minutes > 0 && !this.hasYear && !this.hasMonth && !this.hasDay && !this.hasWeek) {
+      r += this.sep() + this.pluralize(minutes, 'minute');
+    }
+
+    if (this.hasOne) return r + ' ago';
     return 'a few seconds ago';
   },
 
   pluralize: function(count, single, many) {
+    this.hasOne = true;
+
     if (!many) many = single + 's';
     return count  + ' ' + (count == 1 ? single : many);
+  },
+
+  sep: function() {
+    return this.hasOne ? ', ' : '';
+  },
+
+  rewind: function() {
+    this.hasOne   = false;
+    this.hasYear  = false;
+    this.hasMonth = false;
+    this.hasWeek  = false;
+    this.hasDay   = false;
   }
 };
